5bd42c31d4a463c19cf89da8ca23a0afde2cb200
Daniel <adebamipedaniel@gmail.com>
2026-07-31 23:18:31 +0200

    get all tasks and single read task

--- API.js
+++ API.js
@@ -8,6 +8,22 @@ app.get('/', (req, res) => {
     res.send('Task Manager API is running');
 });
 
+
+// Get all tasks
+app.get('/tasks', (req, res) => {
+  res.status(200).json(tasks); // Send array as JSON
+});
+
+//Get single task
+app.get('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const task = tasks.find(task => task.id === id);
+    if (!task) {
+        return res.status(404).json({error: "Task not found"});
+    };
+    res.json(task)
+});
+
 const PORT = process.env.PORT || 3002
 
 app.listen(PORT, () =>{
